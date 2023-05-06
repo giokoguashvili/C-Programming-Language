ac155bd98b77b78264479c39ed16e720843cfe85
Giorgi Koguashvili <kogo27@gmail.com>
2023-05-07 00:51:41 +0400

    added macros

--- asm/Assembly-Language-Tutorial/6. Read-From-Console/main.asm
+++ asm/Assembly-Language-Tutorial/6. Read-From-Console/main.asm
@@ -1,3 +1,18 @@
+SYS_EXIT    equ 1
+SYS_READ    equ 3
+SYS_WRITE   equ 4
+
+STDIN       equ 0
+STDOUT      equ 1
+
+%macro write_string 2
+    mov eax, SYS_WRITE                      ;sys_write(
+    mov ebx, STDOUT                         ;   1,          // int fd           = 1          - Standard output (stdout)
+    mov ecx, %1                             ;   userMsg,    // const void *buf  = userMsg    - second param userMsg
+    mov edx, %2                             ;   5           // size_t count     = lenUserMsg - string length
+    int 80h                                 ;);             // make sys call
+%endmacro
+
 section .data                               ;Data segment
     userMsg db 'Please enter a number: '    ;Ask the user to enter a number
     lenUserMsg equ $-userMsg                ;The length of the message
@@ -10,35 +25,31 @@ section .text                               ;Code Segment
 main:
                                             ;User prompt
                                             ;ssize_t write(int fd, const void *buf, size_t count);
-    mov eax, 4                              ;sys_write(
-    mov ebx, 1                              ;   1,          // int fd           = 1          - Standard output (stdout)
+    mov eax, SYS_WRITE                      ;sys_write(
+    mov ebx, STDOUT                         ;   1,          // int fd           = 1          - Standard output (stdout)
     mov ecx, userMsg                        ;   userMsg,    // const void *buf  = userMsg    - second param userMsg
     mov edx, lenUserMsg                     ;   5           // size_t count     = lenUserMsg - string length
     int 80h                                 ;);             // make sys call
 
                                             ;Read and store the user input
                                             ;ssize_t read(int fd, void *buf, size_t count);
-    mov eax, 3                              ;read(
+    mov eax, SYS_READ                       ;read(
     mov ebx, 2                              ;   2, //
     mov ecx, num                            ;   5, //
     mov edx, 5                              ;   5, // 5 bytes (numeric, 1 for sign) of that information
     int 80h                                 ;);   // make sys call
 
                                             ;Output the message 'The entered number is: '
-    mov eax, 4
-    mov ebx, 1
+    mov eax, SYS_WRITE
+    mov ebx, STDOUT
     mov ecx, dispMsg
     mov edx, lenDispMsg
     int 80h
 
                                             ;Output the number entered
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, num
-    mov edx, 5
-    int 80h
+    write_string num, 5
 
                                             ;Exit code
-    mov eax, 1
+    mov eax, SYS_EXIT
     mov ebx, 0
     int 80h
--- asm/README.md
+++ asm/README.md
@@ -1,12 +1,15 @@
 # Assembly
-
+Linux system calls located in `unistd.h` file
+```
+$ vi /usr/include/asm/unistd.h
+```
+32-bit
 ```as
 $ nasm -f elf main.asm -o main.o 
 $ ld -m elf_i386 -s main.o -o main.out
 $ ./main.out
 ```
-
-
+64-bit
 ```as
 $ nasm -f elf64 hello.asm -o hello.o 
 $ ld hello.o -o hello
